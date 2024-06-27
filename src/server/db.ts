a3a44f7489c5e1549498415b81539db5ed66c3b7
Tellay <tellayprivadobuilds@gmail.com>
2024-06-27 17:34:46 +0100

    fix: update database to PostgreSQL

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,9 +1,15 @@
+import { Pool } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db = globalThis.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
